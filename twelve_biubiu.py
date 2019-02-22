e28095bc06ce6744aeff000d46867818862f452f
CZZLEGEND <185828143@qq.com>
2019-02-22 17:41:21 +0800

    twelve_biubiu.py
    twelve_biubiu.py

---
+++ twelve_biubiu.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+
+def twelve_biubiu():
+    print('发送机正在启动...')
+    for i in range(12):
+        print('Biu!!')
+    print('\--/')
+    print('|  |')
+    print('|  |')
+    print('|  |')
+    print('|  |')
+    print('|  |')
+    print('|  |')
+    print('|  |')
+    print('/---\\')
+    print('/----\\')
+    print('////\\\\\\\\')
+
+
+twelve_biubiu()
